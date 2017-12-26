45cce58af6b7e5a0817d94fde3e822eb62d65753
Tim Staley <git@timstaley.co.uk>
2017-12-26 18:09:36 +0000

    Add 2016-11 through 2017-06.
    Rename 2016-01 through 2016-08 for double-digits (zero-padding month numeral).

--- dumpscript.sh
+++ dumpscript.sh
@@ -2,8 +2,9 @@ YEAR=2016
 NSPLIT=100000
 STARTMONTH=1
 # Jan - Feb, end date is start of next month:
-for i in $(seq -w $STARTMONTH 11); do 
-    voeventdb_dump_tarball.py -n $NSPLIT -p -e ${YEAR}-$(expr ${i} + 1) -s ${YEAR}-${i}  ${YEAR}-${i}
+for i in $(seq $STARTMONTH 11); do 
+    FILESTEM=${YEAR}-$(printf %02d $(expr ${i} + 1))
+    voeventdb_dump_tarball.py -n $NSPLIT -p -e ${YEAR}-$(expr ${i} + 1) -s ${YEAR}-${i}  ${FILESTEM}
 done
 # For December, end date is start of the next year:
 #voeventdb_dump_tarball.py -n $NSPLIT -p -e $(expr ${YEAR} + 1)-01-01 -s ${YEAR}-12  ${YEAR}-12
