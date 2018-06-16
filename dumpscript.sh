c0bc5f6f93d6540cb1eee8f1521fa39aa7ffb709
Tim Staley <git@timstaley.co.uk>
2018-06-16 21:21:00 +0100

    Modify dump-script

--- dumpscript.sh
+++ dumpscript.sh
@@ -1,10 +1,12 @@
-YEAR=2016
-NSPLIT=100000
+YEAR=2017
+NSPLIT=50000
 STARTMONTH=1
+DATABASE=voeventdb
 # Jan - Feb, end date is start of next month:
-for i in $(seq $STARTMONTH 11); do 
-    FILESTEM=${YEAR}-$(printf %02d $(expr ${i} + 1))
-    voeventdb_dump_tarball.py -n $NSPLIT -p -e ${YEAR}-$(expr ${i} + 1) -s ${YEAR}-${i}  ${FILESTEM}
+for i in $(seq -f %02g $STARTMONTH 11); do 
+    FILESTEM=${YEAR}-${i}
+    voeventdb_dump_tarball.py -d$DATABASE -n $NSPLIT -p -e ${YEAR}-$(expr ${i} + 1) -s ${YEAR}-${i}  ${FILESTEM}
 done
 # For December, end date is start of the next year:
-#voeventdb_dump_tarball.py -n $NSPLIT -p -e $(expr ${YEAR} + 1)-01-01 -s ${YEAR}-12  ${YEAR}-12
+voeventdb_dump_tarball.py -d$DATABASE -n $NSPLIT -p -e $(expr ${YEAR} + 1)-01-01 -s ${YEAR}-12  ${YEAR}-12
+
